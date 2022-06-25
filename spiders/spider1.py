88ebfd7e9bbbf71c0a0db6cd0127a6d39035d1ab
wu1724 <besttao@foxmail.com>
2022-06-25 22:00:58 +0800

    更新api的key

--- spiders/spider1.py
+++ spiders/spider1.py
@@ -3,8 +3,8 @@ import requests
 
 class Spider1():
     def __init__(self):
-        self.base_url = 'https://www.padaili.com/proxyapi?api=wF0MqtsPPiHJJh3xRm2zEH61RvqsbMA3&num=500&type=3' \
-                        '&xiangying=1&order=jiance '
+        self.base_url = 'https://www.padaili.com/proxyapi?api=e6wufLiDIOsXVRm1M5QZ76w3OdXCPW9X&num=500&type=3' \
+                        '&xiangying=1&order=jiance'
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'}
 
     def get_proxys(self):
